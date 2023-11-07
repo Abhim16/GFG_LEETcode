9fb3d6a0b2342b1a0cef45119f5704fc731a5751
Abhim16 <100203367+Abhim16@users.noreply.github.com>
2023-11-07 11:35:47 +0530

    Time: 68 ms (54.48%), Space: 17.8 MB (6.14%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,19 +1,32 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        nums_with_indices = list(enumerate(nums))  # Create a list of tuples (index, value)
-        nums_with_indices.sort(key=lambda x: x[1])  # Sort the list of tuples based on values
-
-        left, right = 0, len(nums) - 1
-
-        while left < right:
-            current_sum = nums_with_indices[left][1] + nums_with_indices[right][1]
-            if current_sum == target:
-                return [nums_with_indices[left][0], nums_with_indices[right][0]]
-            elif current_sum < target:
-                left += 1
+        number = list(enumerate(nums))
+        number.sort(key = lambda x:x[1])
+        left,right = 0,len(nums)-1
+        while(left<right):
+            cur_sum = number[left][1]+number[right][1]
+            if cur_sum == target:
+                return [number[left][0],number[right][0]]
+            elif cur_sum > target:
+                right-=1
             else:
-                right -= 1
+                left+=1
+                
+        
+#         nums_with_indices = list(enumerate(nums))  # Create a list of tuples (index, value)
+        # nums_with_indices.sort(key=lambda x: x[1])  # Sort the list of tuples based on values
 
-        return []        
+#         left, right = 0, len(nums) - 1
+
+#         while left < right:
+#             current_sum = nums_with_indices[left][1] + nums_with_indices[right][1]
+#             if current_sum == target:
+#                 return [nums_with_indices[left][0], nums_with_indices[right][0]]
+#             elif current_sum < target:
+#                 left += 1
+#             else:
+#                 right -= 1
+
+#         return []        
             
         
