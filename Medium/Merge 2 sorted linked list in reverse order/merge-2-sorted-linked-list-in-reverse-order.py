b81b78fba9f40380f65f5214e5b4f04acff25790
Abhim16 <100203367+Abhim16@users.noreply.github.com>
2024-01-08 17:02:28 +0530

    Updated Solution - GfG to GitHub

--- Medium/Merge 2 sorted linked list in reverse order/merge-2-sorted-linked-list-in-reverse-order.py
+++ Medium/Merge 2 sorted linked list in reverse order/merge-2-sorted-linked-list-in-reverse-order.py
@@ -1,10 +1,10 @@
 #User function Template for python3
 
 
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
+# class Node:
+#     def __init__(self, data):
+#         self.data = data
+#         self.next = None
 
 class Solution:
     def mergeResult(self, h1, h2):
